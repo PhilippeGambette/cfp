8d6f8ac037c827dbf68cc2bd9180eb7aa4f64ef9
PhilippeGambette <philippe.gambette@laposte.net>
2024-03-04 07:13:49 +0100

    :lipstick: (whole website) version 0.2 of 20151128
    missing WFC.ico

--- WFC.php
+++ WFC.php
@@ -9,18 +9,36 @@
 
 <SCRIPT TYPE="text/javascript" SRC="jquery-1.11.2.min.js"></script>
 
-<!--SoO/ei5l blg-->
-<SCRIPT TYPE="text/javascript">
+<script type="text/javascript">
+/*
+    ******************** WFC - Word Frequency Chains ********************
+    -> a Javascript code to compare the frequency of words in two texts
+    Copyright (C) 2015 - Philippe Gambette, Nadège Lechevrel
 
-function findKey(needle,haystack){
-   return haystack.indexOf(needle);
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+function normalizeSize(x,min,max){
+   return Math.round(9+15*(Math.log(x)-Math.log(min))/(Math.log(max)-Math.log(min)));
 }
 
-
- 
 $(document).ready(function(){
 
 <?php
+
+//Initialize the two lists of frequent words:
 echo "";
 
 function treat($t,$c1,$c2){
@@ -53,113 +71,175 @@ echo $f1."];\n";
 treat("words1","w1","f1");
 treat("words2","w2","f2");
 
+//Initialize the titles:
+if(isset($_POST["title1"])){
+   echo "var title1=\"".$_POST["title1"]."\";";
+} else {
+   echo "var title1=\"List 1\";";
+}
+
+if(isset($_POST["title2"])){
+   echo "var title2=\"".$_POST["title2"]."\";";
+} else {
+   echo "var title2=\"List 2\";";
+}
+
 ?>   
    
+   //Minimum and maximum number of occurrences in the 1st list (expected to be sorted in decreasing order)
    var min1 = f1[f1.length-1];
    var max1 = f1[0];
    
+   //Minimum and maximum number of occurrences in the 2nd list (expected to be sorted in decreasing order)
    var min2 = f2[f2.length-1];
    var max2 = f2[0];
    
-   var y1=0;
-   var py1 = [];
+   //Initialization of other variables
+   var y1=30;
+   var y2=30;
+   var py1=[];
+   var py2=[];
    var h1=0;
+   var h2=0;
+   
+   //Horizontal position of the left endpoint of the links
    var x1=400;
-   var x2=600;
 
+   //Horizontal position of the right endpoint of the links
+   var x2=600;
+   
+   //Add the titles
+   $("#label").css("font-size","24pt");
+   $("#label").text(title1);
+   $("#panel").append('<text id="title1"'+
+   ' class="link" fill="black" '+
+   'x="'+(x1-5-parseInt($("#label").css("width")))+'" '+
+   'y="30" style="font-family:Calibri,sans-serif;'+
+   'font-size:24pt;">'+title1+"</text>\n");
+   $("#panel").append('<text id="title2"'+
+   ' class="link" fill="black" '+
+   'x="'+(x2+5)+'" '+
+   'y="30" style="font-family:Calibri,sans-serif;'+
+   'font-size:24pt;">'+title2+"</text>\n");
+   
+   //Put the labels of the 1st list
    for (var i=0;i<w1.length;i++){
-      //var label=makeSVG('text',{id:"bo",title:"ol",value:'bla',fill:'black',x:"10",y:i+"",content:"la",text:"bl", "font-size":"12"});
-      //var label=makeTheSVG('',{id:"bo",title:"ol",value:'bla',,content:"la",text:"bl", "font-size":"12"},"bla");
       y1=y1+10+h1;
-      h1=Math.floor(9+15*(f1[i]-min1)/(max1-min1));
+      h1=normalizeSize(f1[i],min1,max1);
       $("#label").css("font-size",h1+"pt");
       $("#label").text(w1[i]);
-      $("#panel").append('<text id="l1'+i+'" class="link" d="'+pathd+'" fill="black" x="'+(x1-5-parseInt($("#label").css("width")))+'" y="'+(h1+y1-1)+'" style="font-family:Calibri,sans-serif;font-size:'+h1+'pt;">'+w1[i]+'</text>');
-      console.log("h"+(h1));
+      $("#panel").append('<text id="l1'+i+'"'+
+      ' class="link" fill="black" '+
+      'x="'+(x1-5-parseInt($("#label").css("width")))+'" '+
+      'y="'+(h1+y1-1)+'" style="font-family:Calibri,sans-serif;'+
+      'font-size:'+h1+'pt;">'+w1[i]+"</text>\n");
+      //console.log("h"+(h1));
       py1.push(y1);
    }
    $("#label").text("");
-   var y2=0;
-   var py2 = [];
-   var h2=0;
+   
    for (var i=0;i<w2.length;i++){
       y2=y2+10+h2;
-      h2=Math.floor(9+15*(f2[i]-min2)/(max2-min2));
-      $("#panel").append('<text id="l2'+i+'" class="link" d="'+pathd+'" fill="black" x="'+(x2+5)+'" y="'+(h2+y2)+'" style="font-family:Calibri,sans-serif;font-size:'+h2+'pt;">'+w2[i]+'</text>');
-      console.log("h"+(h2));
+      h2=normalizeSize(f2[i],min2,max2);
+      $("#panel").append('<text id="l2'+i+'" class="link" fill="black" x="'+(x2+5)+'" y="'+(h2+y2-1)+'" style="font-family:Calibri,sans-serif;font-size:'+h2+'pt;">'+w2[i]+"</text>\n");
+      //console.log("h"+(h2));
       py2.push(y2);
    }
    
-   ystart=0;
-
-   var h1=0;
-   var h2=0;
-   y2=0;
-   
+   // Compute the smallest and highest differences of position in 1st and 2nd list
+   var minHeight=0;
+   var maxHeight=0;
    for (var i=0;i<w1.length;i++){
-      y1=py1[i];
-      var indexInW2=findKey(w1[i],w2);
-      if (indexInW2>-1){
-      y2=py2[indexInW2];
-      h1=Math.floor(9+15*(f1[i]-min1)/(max1-min1));
-      h2=Math.floor(9+15*(f2[indexInW2]-min2)/(max2-min2));
-      console.log("h"+(h1)+"h"+(h2));
-      /*if w2.contains(w1[i]){
-         $("panel").append("<div style=\"float:right;margin-right:500px\"></div>")
-      } else {
-         $("panel").append("<div style=\"float:left;margin-left:500px\"></div>")   
-      }*/
-   
-      var pathd="M"+x1+","+y1+"C"+Math.floor((x1+x2)/2)+","+y1+" "+Math.floor((x1+x2)/2)+","+y2+" "+x2+","+y2+"C"+x2+","+Math.floor((y2+h2)/2)+" "+x2+","+Math.floor((y2+h2)/2)+" "+x2+","+(y2+h2)+"C"+Math.floor((x1+x2)/2)+","+(y2+h2)+" "+Math.floor((x1+x2)/2)+","+(y1+h1)+" "+x1+","+(y1+h1)+"C"+x1+","+Math.floor((y1+h1)/2)+" "+x1+","+Math.floor((y1+h1)/2)+" "+x1+","+y1;
-      
-      $("#panel").append('<path class="link" d="'+pathd+'", style="color:red;stroke-width: 1px;" opacity="0.5";></path>');
-      
-      //var path=makeSVG('path', {class: "link", d:'M30,10C65,10 65,50 100,50C100,65 100,65 100,80C65,80 65,20 30,20C30,15 30,15 30,10', style:'color:red;stroke-width: 1px;'});
-      //var path=makeSVG('path', {class: "link", d:pathd, fill: 'black', style:'opacity:0.5;'});
-      //document.getElementById('s').appendChild(path); 
+      var indexInW2=w2.indexOf(w1[i]);
+      if (i-indexInW2<minHeight){
+         minHeight=i-indexInW2;
+      }
+      if (i-indexInW2>maxHeight){
+         maxHeight=i-indexInW2;
       }
    }
-   $("svg").css("height",Math.max(y1,y2)+10);
+      
+   // Display the links
+   for (var i=0;i<w1.length;i++){
+      y1=py1[i];
+      var indexInW2=w2.indexOf(w1[i]);
+      if (indexInW2>-1){
+         y2=py2[indexInW2];
+         h1=normalizeSize(f1[i],min1,max1);
+         h2=normalizeSize(f2[indexInW2],min2,max2);
+         //console.log("h"+(h1)+"h"+(h2));
+         var pathd="M"+x1+","+y1+
+         "C"+Math.floor((x1+x2)/2)+","+y1+" "+Math.floor((x1+x2)/2)+","+y2+" "+x2+","+y2+
+         "C"+x2+","+Math.floor((y2+h2)/2)+" "+x2+","+Math.floor((y2+h2)/2)+" "+x2+","+(y2+h2)+
+         "C"+Math.floor((x1+x2)/2)+","+(y2+h2)+" "+Math.floor((x1+x2)/2)+","+(y1+h1)+" "+x1+","+(y1+h1)+
+         "C"+x1+","+Math.floor((y1+h1)/2)+" "+x1+","+Math.floor((y1+h1)/2)+" "+x1+","+y1;
+         
+         // Color the links
+         var green=125;
+         var red=125;
+         if (i-indexInW2>0){
+            green=50+Math.floor(205*(i-indexInW2)/(maxHeight))
+            red=0;
+         }else{
+            if (i-indexInW2==0){
+               red=50;
+               green=50;
+            }else{
+               red=50+Math.floor(205*(i-indexInW2)/(minHeight))
+               green=0
+            }
+         }
+         $("#panel").append('<path class="link" d="'+pathd+'" fill="rgb('+red+',0,'+green+')" style="stroke-width:1px" opacity="0.3">'+"</path>\n"); 
+      }
+   }
+   $("svg").css("height",Math.max(y1,y2)+70);
+   $("svg").css("width",x1+x2);
    $("#cont").html($("#cont").html());
-   
-   $(document).on("change","select",function() {
-      updateValues();
-   });
-   
-   $(document).on("change","input",function() {
-      updateValues();
-   });
-    
 });
-    
-</SCRIPT>
+</script>
 
-   <BODY style="font-family:Calibri,sans-serif">
-   <h1>Word frequency chains</h1>
+<body style="font-family:Calibri,sans-serif;margin:20px;">
 
-<span style="width:auto" id="label" style="font-family:Calibri,sans-serif;display:none;"></span>
+<a href="index_fr.php"><img src="LangFrench.jpg" alt="En français..." title="En français..." style="float:right;margin-left:10px;"></a>
+<h1>Word Frequency Chains</h1>
+<p>
+   The visualization below displays the 2 lists of words you have given as input.
+   The <b>sizes of the words</b> depend on their number of occurrences according to the 2 input lists.
+   If the same word appears on the left and on the right side, a <b>link</b> is drawn between the two occurrences:
+   the link is <b>blue</b> if the word is ranked better in the second list than in the first list, <b>red</b> if
+   ranked better in the first one, <b>gray</b> if it has the same rank in both lists.
+</p>
+<p>
+   This visualization helps to detect "<i><b>word frequency chains</b></i>", that is <b>sets of words which are
+   ranked in a reverse order in the first and in the second list</b>. To find such lists, find <b>a blue link
+   intersecting a red link</b>, and identify the links intersecting them whose left endpoints and right endpoints
+   are sorted in symmetric orderings.
+</p>
+
+<span style="width: auto; font-size: 9pt;" id="label"></span>
 <div id="cont">
-<svg style="width:1000px;height:500px" id="panel">
+<svg style="width: 1000px; height: 697px;" id="panel">
 </svg>
 </div>
-<!--
-<path class="firstlink" d="M419.14285714285717,71.42047105822905C681.0714285714287,71.42047105822905 681.0714285714287,101.83666455099315 943,101.83666455099315" style="stroke-width: 83.9581212819503px;"><title>Thermal generation ? Losses
-787 TWh</title></path>
 
-x1=30
-y1=10
-h1=20
+<h2>About</h2>
+<p>
+   WFC - Word Frequency Chains, a visual tool to compare the frequency of words in two texts<br/>
+   Copyright &copy; 2015 - <a href="https://sites.google.com/site/nadegelechevrel/">Nadège Lechevrel</a>
+   &amp; <a href="http://igm.univ-mlv.fr/~gambette/">Philippe Gambette</a>.
+</p>
+<p>
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+</p>
+<p>
+   This program is distributed in the hope that it will be useful,
+   but without any warranty; without even the implied warranty of
+   merchantability or fitness for a particular purpose.  See the
+   <a href="http://www.gnu.org/licenses/">GNU General Public License</a> for more details.
+</p>
 
-x2=100
-y2=50
-h2=30
-
-<path class="link" d="Mx1,y1C(x1+x2)/2,y1 (x1+x2)/2,y2 x2,y2Cx2,(y2+h2)/2 x2,(y2+h2)/2 x2,y2+h2C(x1+x2)/2,y2+h2 (x1+x2)/2,y1+h1 x1,y1+h1Cx1,(y1+y2)/2 x1,(y1+y2)/2 x1,y1+h1" style="color:red;stroke-width: 1px;"><title>BimBoum</title></path>-->
-
-
-<h2>&Agrave; propos</h2>
-
-Outil conçu par <a href="https://sites.google.com/site/nadegelechevrel/">Nadège Lechevrel</a> et <a href="http://igm.univ-mlv.fr/~gambette/">Philippe Gambette</a>.
-
-   </BODY>
-</HTML>
+<!--SoO/ei5l blg-->
+</body></html>
---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+      <TITLE>Word Frequency Chains</TITLE>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.5/united/bootstrap.min.css">
+      <link rel="icon" href="WFC.ico" />
+</head>
+
+<body style="font-family:Calibri,Arial,sans-serif">
+<div style="margin:20px;">
+<a href="index_fr.php"><img src="LangFrench.jpg" alt="En français..." title="En français..." style="float:right;margin-left:10px;"></a>
+<h1>Word Frequency Chains: comparing 2 lists of most frequent words</h1>
+
+Please insert below the lists of most frequent words of two texts you want to visualize. On each line, write the word first and then its number of occurrences.
+You can build such lists using the <a href="http://www.treecloud.org">Treecloud.org</a> website.
+
+<form action="WFC.php" method="post">
+<br/>
+<div style="float:left;width:auto;margin:20px;padding:20px;background-color:#AAEEAA">
+Name of the first list:<br/>
+<input type="text" name="title1"></input><br/><br/>
+Most frequent words of the 1st text:<br/>
+<textarea name="words1" col=10 rows=10>
+species;3499
+biodiversity;1044
+populations;1027
+genetic;901
+environmental;868
+important;857
+plant;838
+population;798
+change;789
+effects;743
+natural;731
+diversity;726
+climate;695
+management;680
+processes;647
+soil;643
+water;614
+marine;611
+analysis;589
+ecosystem;587
+ecological;580
+ecosystems;578
+understanding;567
+areas;557
+development;544
+model;542
+plants;526
+models;523
+environment;500
+distribution;493
+system;487
+molecular;479
+conditions;479
+evolution;474
+role;474
+</textarea><br/>
+</div>
+<div style="float:left;width:auto;margin:20px;padding:20px;background-color:#EEAAAA">
+Name of the second list:<br/>
+<input type="text" name="title2"></input><br/><br/>>
+Most frequent words of the 2nd text:<br/>
+<textarea name="words2" col=10 rows=10>
+species;4025
+change;1485
+climate;1412
+environmental;1337
+biodiversity;1258
+genetic;1177
+populations;1151
+ecosystem;1136
+management;1108
+soil;1103
+important;1084
+plant;1067
+diversity;1063
+effects;984
+population;919
+water;917
+natural;913
+understanding;913
+ecological;896
+ecosystems;865
+carbon;830
+marine;805
+potential;767
+environment;763
+models;755
+model;749
+plants;733
+processes;713
+global;709
+conservation;705
+development;686
+conditions;685
+range;684
+communities;681
+large;662
+</textarea><br/>
+</div>
+<br>
+<input type="submit" value="Submit">
+<br/>
+<INPUT type="checkbox" name="frequencies" value="Display frequencies">Display frequencies
+</form>
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+<h2>About</h2>
+<p>
+   WFC - Word Frequency Chains, a visual tool to compare the frequency of words in two texts<br/>
+   Copyright &copy; 2015 - <a href="https://sites.google.com/site/nadegelechevrel/">Nadège Lechevrel</a>
+   &amp; <a href="http://igm.univ-mlv.fr/~gambette/">Philippe Gambette</a>.
+</p>
+<p>
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+</p>
+<p>
+   This program is distributed in the hope that it will be useful,
+   but without any warranty; without even the implied warranty of
+   merchantability or fitness for a particular purpose.  See the
+   <a href="http://www.gnu.org/licenses/">GNU General Public License</a> for more details.
+</p>
+
+</div>
+</body></html>
